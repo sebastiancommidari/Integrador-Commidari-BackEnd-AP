1f822d12067a4b9544d3d427d12a3af42548220c
Sebastián Commidari (PC Secundaria) <sebastian.commidari@gmail.com>
2022-08-20 05:12:02 -0300

    Creación de Backend con Spring Initializr y conexión con BBDD

---
+++ src/main/java/com/porfolio/commidari/CommidariApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.commidari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommidariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommidariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfolio/commidari/CommidariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.commidari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommidariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
